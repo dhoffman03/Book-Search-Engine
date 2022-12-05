32118816d8485ba8c73ec47e0609a7f1c3515f18
Dymond <dymond.hoffman03@gmail.com>
2022-12-05 12:35:13 -0800

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -2,16 +2,13 @@ const express = require('express');
 // Import Apollo server
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// const routes = require('./routes');
-
+const { authMiddleware } = require('./utils/auth');
 // Import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-
 const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,8 +16,7 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -28,9 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 
