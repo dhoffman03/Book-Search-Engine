718dc4657b122652646cb02dcc367c3603bf739c
Dymond <dymond.hoffman03@gmail.com>
2022-12-05 12:14:53 -0800

    addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -29,11 +29,13 @@ const resolvers = {
             }
 
             const token = signToken(user);
-            res.json({ token, user });
+            return { token, user };
         },
 
         addUser: async (parent, { username, email, password }) => {
-
+            const user = await User.create({ username, email, password })
+            const token = signToken(user);
+            return { token, user };
         },
 
         saveBook: async (parent, { newBook }, context) => {
