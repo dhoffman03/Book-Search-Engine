b282d46cdc21274d238ea8cd0fe442d8d4480f79
Dymond <dymond.hoffman03@gmail.com>
2022-12-04 23:10:52 -0800

    Import typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const routes = require('./routes');
-// 
-const { authMiddleware } = require('./utils/auth')
+
+// Import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
